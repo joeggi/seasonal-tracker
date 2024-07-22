76cab2ecd500ad60c536d7c7936cdfea2055b44a
joeggi <morgan.petersen2020@gmail.com>
2024-07-22 16:59:15 -0400

    added initial api call to jikan and index html file

---
+++ jikan_api_calls.py
@@ -0,0 +1,38 @@
+from __future__ import print_function
+import requests
+import sys
+from flask import Flask, jsonify, send_from_directory
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/data')
+def get_mal_data():
+    # Define the API endpoint URL
+    url = 'https://api.jikan.moe/v4/users/nickelkami/full'
+
+    print("fetching api data", file=sys.stderr)
+
+    try:
+        # Make a GET request to the API endpoint using requests.get()
+        response = requests.get(url)
+
+        # Check if the request was successful (status code 200)
+        if response.status_code == 200:
+            posts = response.json()
+            return jsonify(posts)
+        else:
+            print('Error:', response.status_code)
+            return None
+        
+    except requests.exceptions.RequestException as e:
+        print('Error:', e)
+        return None
+    
+@app.route('/')
+def index():
+    return send_from_directory('.', 'index.html')
+    
+if __name__ == '__main__':
+    app.run(debug=True)
